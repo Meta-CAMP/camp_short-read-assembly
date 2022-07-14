8ff3a675d75ddba3fa590dce55983d4cc5bdf3f4
Braden T Tierney <btt4001@phobos.pbtech>
2022-07-14 10:28:11 -0400

    assembly init commit

--- workflow/utils.py
+++ workflow/utils.py
@@ -11,18 +11,15 @@ import pandas as pd
 
 
 def ingest_samples(samples, tmp):
-    df = pd.read_csv(samples, header = 0, index_col = 0) # name, ctgs, fwd, rev
+    df = pd.read_csv(samples, header = 0, index_col = 0) 
     s = list(df.index)
     lst = df.values.tolist()
     for f in os.listdir(tmp):
-        os.remove(join(tmp, f))
+        os.system("rm -rf " + join(tmp, f))
     for i,l in enumerate(lst):
-        # Symlink your original data to the temporary directory
-        # Example: symlink(l[0], join(tmp, s[i] + '.fasta'))
-        # Example: symlink(l[1], join(tmp, s[i] + '_1.fastq'))
-        # Example: symlink(l[2], join(tmp, s[i] + '_2.fastq'))
+        symlink(l[0], join(tmp, s[i] + '_1.fastq'))
+        symlink(l[1], join(tmp, s[i] + '_2.fastq'))
     return s
-
 
 class Workflow_Dirs:
     '''Management of the working directory tree.'''
@@ -31,13 +28,11 @@ class Workflow_Dirs:
     LOG = ''
 
     def __init__(self, work_dir, module):
-        self.OUT = join(work_dir, short-read-assembly)
+        self.OUT = join(work_dir, "short-read-assembly")
         self.TMP = join(work_dir, 'tmp') 
         self.LOG = join(work_dir, 'logs') 
         if not exists(self.OUT):
             makedirs(self.OUT)
-            # Add custom subdirectories to organize intermediate files
-            makedirs(join(self.OUT, 'final_reports'))
         if not exists(self.TMP):
             makedirs(self.TMP)
         if not exists(self.LOG):
